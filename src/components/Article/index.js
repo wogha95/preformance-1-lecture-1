9c45c2bf02d6a52426edb0a9e4a88a2f03238bf1
wogha95 <wogha95@naver.com>
2024-07-17 22:16:08 +0900

    1-7) bottleneck 코드 최적화

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -17,23 +17,24 @@ function getParametersForUnsplash({width, height, quality, format}) {
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
 function removeSpecialCharacter(str) {
-  const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
-  let _str = str
-  let i = 0,
-    j = 0
+  return str.substring(0, 300).replace(/[\#\_\*\~\&\;\!\[\]\`\>\\n\=\-]/g, '')
+  // const removeCharacters = ['#', '_', '*', '~', '&', ';', '!', '[', ']', '`', '>', '\n', '=', '-']
+  // let _str = str
+  // let i = 0,
+  //   j = 0
 
-  for (i = 0; i < removeCharacters.length; i++) {
-    j = 0
-    while (j < _str.length) {
-      if (_str[j] === removeCharacters[i]) {
-        _str = _str.substring(0, j).concat(_str.substring(j + 1))
-        continue
-      }
-      j++
-    }
-  }
+  // for (i = 0; i < removeCharacters.length; i++) {
+  //   j = 0
+  //   while (j < _str.length) {
+  //     if (_str[j] === removeCharacters[i]) {
+  //       _str = _str.substring(0, j).concat(_str.substring(j + 1))
+  //       continue
+  //     }
+  //     j++
+  //   }
+  // }
 
-  return _str
+  // return _str
 }
 
 function Article(props) {
